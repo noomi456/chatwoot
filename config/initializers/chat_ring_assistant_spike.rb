390956e387278f1e38d19729215cf609f918bd41
cqalerts3-code <cqalerts3@gmail.com>
2026-08-06 18:25:37 -0700

    style(assistant): satisfy integrated CE lint

--- config/initializers/chat_ring_assistant_spike.rb
+++ config/initializers/chat_ring_assistant_spike.rb
@@ -1,7 +1,7 @@
 Rails.application.config.to_prepare do
-  unless MessageTemplates::HookExecutionService.ancestors.include?(ChatRing::AssistantSpike::HookExtension)
+  unless MessageTemplates::HookExecutionService <= ChatRing::AssistantSpike::HookExtension
     MessageTemplates::HookExecutionService.prepend(ChatRing::AssistantSpike::HookExtension)
   end
 
-  Message.prepend(ChatRing::AssistantSpike::MessageExtension) unless Message.ancestors.include?(ChatRing::AssistantSpike::MessageExtension)
+  Message.prepend(ChatRing::AssistantSpike::MessageExtension) unless Message <= ChatRing::AssistantSpike::MessageExtension
 end
