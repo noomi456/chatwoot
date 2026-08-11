843eefe7836e29b73ccce4e750db060b6fada988
cqalerts3-code <cqalerts3@gmail.com>
2026-08-11 12:15:42 -0700

    test(ai): select reloaded commit jobs by class name

--- spec/jobs/chat_ring/outbound_commit_job_spec.rb
+++ spec/jobs/chat_ring/outbound_commit_job_spec.rb
@@ -72,7 +72,7 @@ RSpec.describe ChatRing::OutboundCommitJob, type: :job do
       allow(ChatRing::Knowledge::Retriever).to receive(:active_index_id).and_return(nil)
 
       expect(ChatRing::Knowledge::Retriever).not_to receive(:retrieve)
-      perform_enqueued_jobs do
+      perform_enqueued_jobs(only: ->(job) { job.class.name == described_class.name }) do
         ChatRing::AiTurnJob.perform_now(playbook_turn.id)
       end
 
