80a5692178cc745fc50a4520f1321867d7e4a923
cqalerts3-code <cqalerts3@gmail.com>
2026-08-11 03:51:27 -0700

    test(ai): compare serialized schema keys

--- spec/services/chat_ring/brain/decision_schema_spec.rb
+++ spec/services/chat_ring/brain/decision_schema_spec.rb
@@ -4,17 +4,19 @@ RSpec.describe ChatRing::Brain::DecisionSchema do
   it 'uses OpenAI strict-schema-compatible nullable fields' do
     schema = described_class.new.to_json_schema.deep_stringify_keys.fetch('schema')
 
-    expect(schema.fetch('required')).to contain_exactly(
+    expect(schema.fetch('required').map(&:to_s)).to contain_exactly(
       'decision_type', 'response_text', 'reason_code', 'evidence_ids', 'tool_request', 'playbook_control'
     )
     expect(schema.dig('properties', 'tool_request', 'anyOf').last).to eq('type' => 'null')
     expect(schema.dig('properties', 'playbook_control', 'anyOf').last).to eq('type' => 'null')
 
     tool = schema.dig('properties', 'tool_request', 'anyOf').first
-    expect(tool.fetch('required')).to contain_exactly('key', 'version', 'arguments')
-    expect(tool.dig('properties', 'arguments', 'required')).to contain_exactly('requested_time_window', 'reason_code')
+    expect(tool.fetch('required').map(&:to_s)).to contain_exactly('key', 'version', 'arguments')
+    expect(tool.dig('properties', 'arguments', 'required').map(&:to_s)).to contain_exactly(
+      'requested_time_window', 'reason_code'
+    )
 
     playbook = schema.dig('properties', 'playbook_control', 'anyOf').first
-    expect(playbook.fetch('required')).to contain_exactly('action', 'answer_value')
+    expect(playbook.fetch('required').map(&:to_s)).to contain_exactly('action', 'answer_value')
   end
 end
